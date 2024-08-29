0796cd70cf3790b69148242c3a5a1ed5e5523ebd
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-08-29 11:26:11 +0100

    Fixed issues with SSR in NewIssuePage

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { Button, Callout, TextField } from '@radix-ui/themes';
-import 'easymde/dist/easymde.min.css';
-import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
-import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false
+});
 
 interface IssueForm {
   title: string;
