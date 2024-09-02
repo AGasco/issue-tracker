bccd60984dafd075f2417cdae988cbf23aa72863
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-02 15:21:12 +0100

    Extract user fetching into a hook

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,22 +1,24 @@
 'use client';
+import { Skeleton } from '@/app/components';
 import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Skeleton } from '@/app/components';
 import toast, { Toaster } from 'react-hot-toast';
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: () => axios.get<User[]>('/api/users').then((res) => res.data),
+  staleTime: 60 * 1000, // 60s
+  retry: 3
+});
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
     isLoading
-  } = useQuery({
-    queryKey: ['users'],
-    queryFn: () => axios.get<User[]>('/api/users').then((res) => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3
-  });
+  } = useUsers();
 
   if (isLoading) return <Skeleton />;
   if (error) return null;
