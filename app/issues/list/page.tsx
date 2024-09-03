54dfde269cbc8a8ca34c5fabf1f2d323c9417c68
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-03 13:34:41 +0100

    Added functionality to sort by either asc or dec order

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,7 +1,7 @@
 import { IssueStatusBadge, Link } from '@/app/components';
 import prisma from '@/prisma/client';
 import { Issue, Status } from '@prisma/client';
-import { ArrowUpIcon } from '@radix-ui/react-icons';
+import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons';
 import { Table } from '@radix-ui/themes';
 import NextLink from 'next/link';
 import IssueActions from './IssueActions';
@@ -13,24 +13,27 @@ interface Props {
   };
 }
 
-const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: {
-    label: string;
-    value: keyof Issue;
-    className?: string;
-  }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' }
-  ];
+const columns: {
+  label: string;
+  value: keyof Issue;
+  className?: string;
+}[] = [
+  { label: 'Issue', value: 'title' },
+  { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+  { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' }
+];
 
+const IssuesPage = async ({ searchParams }: Props) => {
   const status = Object.values(Status).includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderDirection = searchParams.orderBy?.includes('-') ? 'desc' : 'asc';
+  const orderParam = searchParams.orderBy?.replace('-', '');
   const orderBy = columns
     .map((column) => column.value)
-    .includes(searchParams.orderBy)
-    ? { [searchParams.orderBy]: 'asc' }
+    .includes(orderParam as keyof Issue)
+    ? { [orderParam]: orderDirection }
     : undefined;
 
   const issues = await prisma.issue.findMany({ where: { status }, orderBy });
@@ -41,20 +44,34 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            {columns.map((col) => (
-              <Table.ColumnHeaderCell key={col.value} className={col.className}>
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: col.value }
-                  }}
+            {columns.map((col) => {
+              const orderBySameColumn = orderParam === col.value;
+              const newSortOrder =
+                orderBySameColumn && orderDirection === 'asc'
+                  ? `-${col.value}`
+                  : col.value;
+
+              return (
+                <Table.ColumnHeaderCell
+                  key={col.value}
+                  className={col.className}
                 >
-                  {col.label}
-                </NextLink>
-                {col.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
+                  <NextLink
+                    href={{
+                      query: { ...searchParams, orderBy: newSortOrder }
+                    }}
+                  >
+                    {col.label}
+                  </NextLink>
+                  {col.value === orderParam && orderDirection === 'asc' && (
+                    <ArrowUpIcon className="inline" />
+                  )}
+                  {col.value === orderParam && orderDirection === 'desc' && (
+                    <ArrowDownIcon className="inline" />
+                  )}
+                </Table.ColumnHeaderCell>
+              );
+            })}
           </Table.Row>
         </Table.Header>
         <Table.Body>
