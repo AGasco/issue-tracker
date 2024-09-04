be9953ca358ea252d2a70e317c8abda2e05166ca
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-04 10:52:56 +0100

    Implementd IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import LatestIssues from './LatestIssues';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const openIssueCount = await prisma.issue.count({
+    where: { status: 'OPEN' }
+  });
+  const inProgressIssueCount = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' }
+  });
+  const closedIssueCount = await prisma.issue.count({
+    where: { status: 'CLOSED' }
+  });
+
+  return (
+    <IssueSummary
+      open={openIssueCount}
+      inProgress={inProgressIssueCount}
+      closed={closedIssueCount}
+    />
+  );
 }
